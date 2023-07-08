7beac411d2c702ec5fa8203b9ddbe31f81828a79
YHD97 <ryumkin.ad@gmail.com>
2023-07-08 17:25:12 +0300

    FIX: GUI

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ OPTIONS = [
     "Dec"
 ]
 
+
 def api():
-    date = OPTIONS.index(variable.get())
+    date = OPTIONS.index(date_day.get())
 
     if (date + 1) // 10 == 0:
         date = "0" + str(date + 1)
@@ -49,6 +50,20 @@ def api():
     pp.savefig("tavg.png")
     pp.show()
 
+    img = PhotoImage(file='tavg.png')
+    canv.create_image(0, 0, anchor=NW, image=img)  # TODO:test on another os
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    rendered_page = template.render()
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
 
 
 
@@ -65,50 +80,41 @@ if __name__ == '__main__':
     tk = Tk()
     tk.resizable(False, False)
     tk.title("IDZ")
-    tk.minsize(600 , 450)  # минимальные размеры: ширина - 200, высота - 150
+    tk.minsize(800 , 600)  # минимальные размеры: ширина - 200, высота - 150
     tk.maxsize(1440, 1024)  # максимальные размеры: ширина - 400, высота - 300
 
-    variable = StringVar(tk)
-    variable.set(OPTIONS[0])  # default value
+    date_day = StringVar(tk)
+    date_day.set(OPTIONS[0])
+    label_select_text = ttk.Label(text="Выберите месяц")
+    w = OptionMenu(tk, date_day, *OPTIONS)
 
-    w = OptionMenu(tk, variable, *OPTIONS)
-    w.pack()
-    button = Button(tk, text="OK", command=api)
-    button.pack()
-    label_tavg = ttk.Label()
-    label_tavg.pack()
+    button = Button(tk, text="Enter", command=api)
 
-    label_tmin = ttk.Label()
-    label_tmin.pack()
+    label_tavg_text = ttk.Label(text="Средняя температура в месяце")
+    label_tavg = ttk.Label(text="???")
+    label_tmin_text = ttk.Label(text="Наименьшая температура в месяце")
+    label_tmin = ttk.Label(text="???")
 
-
-    canv = Canvas(tk, width=500, height=500, bg='white')
+    canv = Canvas(tk, width=640, height=480, bg='grey')
     canv.grid(row=2, column=3)
 
-    # ✅ changed the image to .png
-    img = PhotoImage(file="./tavg.png")
-
-    canv.create_image(0, 0, anchor=NW, image=img)
+    label_select_text.pack()
+    w.pack()
+    button.pack()
+    label_tavg_text.pack()
+    label_tavg.pack()
+    label_tmin_text.pack()
+    label_tmin.pack()
+    canv.pack()
     tk.mainloop()
 
 
 
 
 
-"""
-    
-    
-    print(tavg)
-    print(tmin)
-    print(min(tmin))
-    
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html'])
-    )
-    template = env.get_template('index.html')
 
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-"""
+
+
+
+
